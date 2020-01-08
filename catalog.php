2ea1dbc04d7599c332f67c55f2979708b895d84f
Mohit Kumar Mohit Kumar <mmohitku@isimtl.local>
2020-01-08 16:10:18 -0500

    First Commit

---
+++ catalog.php
@@ -0,0 +1,17 @@
+<?php
+
+$page_title = 'Catalog';
+require_once('views/page_top.php');
+
+?>
+
+
+<main>
+    
+</main>
+
+<?php
+
+require_once('views/page_bottom.php');
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$page_title = 'Home';
+require_once('views/page_top.php');
+
+?>
+
+
+<main>
+    
+</main>
+
+<?php
+
+require_once('views/page_bottom.php');
+
+?>
